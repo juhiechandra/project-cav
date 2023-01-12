e81eeebe4fa006fa87d73a3ca385b9e3712749eb
Aarush_009 <aarushkumarchoubey@gmail.com>
2023-01-12 19:13:12 +0530

    14th commit

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -60,7 +60,7 @@
     box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
     box-sizing: border-box;
     width: 100%;
-    font: bold 17px sans-serif;
+    font: bold 16px sans-serif;
     text-align: left;
     padding: 50px 60px 40px;
     overflow: hidden;
